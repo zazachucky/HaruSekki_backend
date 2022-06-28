25b4f2d030dda3cc3ae4383c4a2e2dcb1c011ba6
zazachucky <zazachucky@naver.com>
2022-06-28 20:59:30 +0900

    good

---
+++ src/main/java/com/harusekki/syjmsh/harusekkiApplication.java
@@ -0,0 +1,13 @@
+package com.harusekki.syjmsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class harusekkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(harusekkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harusekki/syjmsh/SyjmshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harusekki.syjmsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyjmshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
